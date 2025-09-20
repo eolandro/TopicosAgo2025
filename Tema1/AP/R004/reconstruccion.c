a3602da18f6f3ec512982cbbd75734f90bf529bd
Misael <l21590290@sjuanrio.tecnm.mx>
2025-09-19 18:42:16 -0600

    Update reconstruccion.c

--- Tema1/AP/R004/reconstruccion.c
+++ Tema1/AP/R004/reconstruccion.c
@@ -13,14 +13,14 @@ int main() {
     srand((unsigned int)current_time);
 
     // Crear archivo PPM en modo escritura binaria
-    file = fopen("sample.ppm", "wb");
+    file = fopen("reconstruido.ppm", "wb");
     if (file == NULL) {
         printf("Error: No se pudo crear el archivo\n");
         return 1;
     }
 
-    // Escribir header PPM (15 bytes)
-    fwrite("P6 640 480 255 ", 1, 15, file);
+    // Escribir header PPM CORRECTO (14 bytes con saltos de línea)
+    fputs("P6\n640 480\n255\n", file);
 
     // Generar píxeles aleatorios (640x480)
     for (int y = 0; y < 480; y++) {
@@ -44,7 +44,8 @@ int main() {
 
     // Cerrar archivo
     fclose(file);
-    printf("Imagen sample.ppm generada con éxito!\n");
+    printf("Imagen reconstruido.ppm generada con éxito!\n");
     
     return 0;
 }
+
